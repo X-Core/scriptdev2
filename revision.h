bf16cded649df6dc81c0c8205c8814e7b844fc4c
unknown <sianne@.(none)>
2010-04-27 00:02:29 +0200

    build fix 0002

---
+++ revision.h
@@ -0,0 +1,6 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define REVISION_ID "cf025dfeefa2638e5e2dee461c72347546f96a45"
+ #define REVISION_DATE "*"
+ #define REVISION_TIME "*"
+#endif // __REVISION_H__
